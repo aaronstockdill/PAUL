0388f74cb0f039e3476f8896cbf8a78227dcf13b
Aaron Stockdill <aaronstockdill@me.com>
2013-06-07 22:22:36 +1200

    Volume issue
    Fix a bug where the volume would increase when lowering was asked.

--- brain.py
+++ brain.py
@@ -1,5 +1,6 @@
 from os import system
 from time import *
+from random import choice
 
 increase = ["up", "increase", "raise", "more"]
 decrease = ["down", "lower", "decrease", "less"]
@@ -13,17 +14,22 @@ def process(command):
     words = command.lower().split()
     if any(word in volume for word in words):
         if any(word in increase for word in words):
-            result = "I'm afraid I can't increase the volume yet."
+            result = "I've increased the volume."
+            system("osascript -e \"set volume output volume (output volume of (get volume settings) + 6.25)\"")
         elif any(word in decrease for word in words):
-            result = "Sorry, I can't lower the volume!"
+            result = "I've lowered the volume."
+            system("osascript -e \"set volume output volume (output volume of (get volume settings) - 6.25)\"")
         elif any(word in mute for word in words):
-            result = "Oops, no muting abilities yet."
+            result = "I've muted the sound."
+            system("osascript -e \"set volume output volume 0\"")
         else:
             result = "I don't understand what you want me to do with the volume."
     elif any(word in mute for word in words):
-        result = "I can't mute the computer yet."
+        result = "The computer has been muted."
+        system("osascript -e \"set volume output volume 0\"")
     elif any(word in greet for word in words):
-        result = "Hi!"
+        print(choice(greet))
+        result = (choice(greet)).title()
     elif any(word in math for word in words):
         result = "The answer is {0}".format(eval(words[1]))
     elif any(word in time for word in words):
